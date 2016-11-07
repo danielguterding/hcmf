Corrected sign in sparse implementation.

HeisenbergMeanField.py
@@ -96,7 +96,7 @@ class HeisenbergMeanFieldCalculator:
     for j,s in enumerate(self.basisstates):
       i.append(j) 
       magterms.append(np.sum(np.multiply(s.get_sz_site_resolved(), self.sitemag)))
-    A = A + spsparse.coo_matrix((magterms,(i,i)), shape=(self.nbasisstates, self.nbasisstates)).tocsr()
+    A = A - spsparse.coo_matrix((magterms,(i,i)), shape=(self.nbasisstates, self.nbasisstates)).tocsr()
     #initialize eigensolver  
     k = 1 #find only ground state
     self.eigenvalues, self.eigenvectors = splinalg.eigsh(A, k=k, which='SA')
@@ -108,9 +108,9 @@ class HeisenbergMeanFieldCalculator:
     addenergy = 0.0
     for b in self.MeanFieldBonds:
       addenergy -= 0.5*self.exchange_parameters[b.Jidx]*self.newsitemag[b.sr]*self.newsitemag[b.sm]
-    self.energy_per_site = self.eigenvalues[0] + addenergy/float(self.nsites)
+    self.energy_per_site = (self.eigenvalues[0] + addenergy)/float(self.nsites)
   def get_magnetization_difference_measure_and_mix_magnetization(self):
-    print self.sitemag
+    #print self.sitemag
     #print self.newsitemag
     convparam = np.linalg.norm(self.newsitemag - self.sitemag)/self.nsites
     mixingfac = 0.45
@@ -118,15 +118,16 @@ class HeisenbergMeanFieldCalculator:
     return convparam
   def solve_selfconsistently(self):
     threshold = 1e-5
-    maxiter = 1000
+    maxiter = 100
     itcounter = 0
     while True:
       self.solve_cluster()
       self.calculate_new_magnetization()
       self.calculate_new_energy_per_site()
       convparam = self.get_magnetization_difference_measure_and_mix_magnetization()
-      #print 'Iteration: %i\nConvergence parameter: %f' % (itcounter, convparam)
-      print 'GS energy: % f' % self.energy_per_site
+      print 'Iteration: %i\nConvergence parameter: %f' % (itcounter, convparam)
+      #print 'GS energy: % f' % self.energy_per_site
+      #print self.sitemag
       itcounter += 1
       if(convparam < threshold):
         print 'Self-consistent loop converged after %i iterations.' % itcounter